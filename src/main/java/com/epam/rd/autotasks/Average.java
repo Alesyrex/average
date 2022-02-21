edcc5b3968570f8d8827c9e1843da191570c2405
Alesyrex <Alesyrex@gmail.com>
2022-02-21 19:26:01 +0300

    create average

--- src/main/java/com/epam/rd/autotasks/Average.java
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -4,9 +4,28 @@ import java.util.Scanner;
 
 public class Average {
     public static void main(String[] args) {
+        final int lengthOfArray = 100;
         Scanner scanner = new Scanner(System.in);
-        // Use Scanner methods to read input
+        int[] arrayForAverage = new int[lengthOfArray];
 
+        int average;
+        int sum = 0;
+        int count = 0;
+
+        while (count < arrayForAverage.length) {
+            arrayForAverage[count] = scanner.nextInt();
+            if (arrayForAverage[count] == 0) {
+                break;
+            }
+            sum += arrayForAverage[count];
+            ++count;
+        }
+
+        if (count != 0) {
+            average = sum / count;
+            System.out.println(average);
+        } else {
+            System.out.println("0");
+        }
     }
-
 }
